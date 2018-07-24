10038dc76c8df88325a11b8ee296e0b83e0e63c4
blooby <adrian.kuper@hotmail.de>
2018-07-24 21:36:18 +0200

    fixed incorrect file paths to jwt certs

--- services/secrets.js
+++ services/secrets.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 
 exports.init = () => {
-  const publicJwtCert = fs.readFileSync('../secrets/jwt-certs/ll-blog-jwt-cert.pem');
+  const publicJwtCert = fs.readFileSync(`${__dirname}/../secrets/jwt-certs/ll-blog-jwt-cert.pem`);
   if (!publicJwtCert) {
     throw new Error('No public certificate for jwt');
   }
 
-  const privateJwtKey = fs.readFileSync('../secrets/jwt-certs/ll-blog-jwt-key.pem');
+  const privateJwtKey = fs.readFileSync(`${__dirname}/../secrets/jwt-certs/ll-blog-jwt-key.pem`);
   if (!privateJwtKey) {
     throw new Error('No private key for jwt');
   }
