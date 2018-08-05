60a19f31a2515e5f69227d701c5fa96111de266b
Adrian Kuper <adrian.kuper@hotmail.de>
2018-08-05 10:25:26 +0200

    Logging unauthorized response

--- middleware/checkAccess.js
+++ middleware/checkAccess.js
@@ -3,8 +3,10 @@ const logger = require('../services/logger').getLogger();
 module.exports = /* options => */ (req, res, next) => {
   logger.debug('Entered middleware: checkAccess');
 
+  // check fields populated by parseAuthInfo middleware
   if (!req.loglevel.auth.user) {
-    res.status(401).send();
+    logger.debug('Unauthorized');
+    res.status(401).send('Unauthorized');
     return;
   }
   // TODO: if more than one users can register: add permission check by
